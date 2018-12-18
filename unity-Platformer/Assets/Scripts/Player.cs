a264f3c3305ffd68fe129071debc2842cd69cfb0
Gaetan Blaise-Cazalet <gblaisec@artfx.fr>
2018-12-18 10:02:27 +0100

    2nd lesson: platformer. Starts project.

---
+++ unity-Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Controller))]
+public class Player : MonoBehaviour {
+
+	Controller controller;
+	
+	void Start () {
+		controller = GetComponent<Controller>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
